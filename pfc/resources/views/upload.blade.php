64b16a1b9f500766eed5cd8363fae15cb48bd12b
Leandro Alves <Leandro.alves351@gmail.com>
2019-03-13 16:38:55 -0300

    Upload de arquivo

--- pfc/resources/views/upload.blade.php
+++ pfc/resources/views/upload.blade.php
@@ -1,4 +1,32 @@
-<form action="POST" method="post" enctype="multipart/form-data"></form>
-    <input type="text" name="nome">
-    <input type="file" name="file">
+<link rel="stylesheet" href="css/app.css">
+
+<form action="/register" method="post" enctype="multipart/form-data">
+    @csrf   
+
+    <div class="container">
+        <form action="/funcionario/cadastrar" method="POST">
+          @csrf
+            <div class="form-group">
+              <label>Nome funcionário</label>
+              <input type="text" class="form-control" name="nome">
+            </div>
+      
+            <div class="form-group">
+                <label>Nome funcionário</label>
+                    <select class="form-control" name="setor">
+                        <option value="adm">Administração</option>
+                        <option value="rh">Recursos Humanos</option>
+                    </select>
+            </div>
+            <div class="form-group">
+                <input type="text" name="arquivo">
+            </div>
+            <div class="form-group"> 
+                <input type="file" name="file"><br>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+      </div>
+        
+    </div>
 </form>
