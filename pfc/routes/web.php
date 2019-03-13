216b3045f833311c9c49b117536d37e63e5d2e5a
Leandro Alves <Leandro.alves351@gmail.com>
2019-03-13 16:39:48 -0300

    Rotas de upload adicionada

--- pfc/routes/web.php
+++ pfc/routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'FileUploadController@cadastroDocumento');
 
-Route::get('/', 'FileUploadController@file');
-
-Route::post('/register', 'FileUploadController@registerFile');
+Route::post('/register', 'FileUploadController@upload');
